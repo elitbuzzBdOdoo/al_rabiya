117e4e0ea7681f78c2f504c04114fae1318d4247
arefin_shawon <arefin.shawon07@gmail.com>
2023-10-08 15:19:17 +0600

    inventory updated

--- inventory_inherit/eb_category_product.py
+++ inventory_inherit/eb_category_product.py
@@ -3,14 +3,24 @@ from odoo import fields, models, api
 
 class EbProductCategory(models.Model):
     _name = 'ebproduct.category'
+    _inherit = ['mail.thread']
+    _order = 'category_id desc'
+    # _rec_name = 'category_name'
     _description = 'Category'
 
-    name = fields.Char(string="Category code", readonly=True)
-    category_name = fields.Char(string="Category")
+    category_id = fields.Char(string="Category code", readonly=True)
+    category_name = fields.Char(string="Category", tracking_visibility='always')
 
     @api.model
     def create(self, vals_list):
         records = super(EbProductCategory, self).create(vals_list)
         for record in records:
-            record.product_template_code = self.env['ir.sequence'].next_by_code('product.template')
+            record.category_id = self.env['ir.sequence'].next_by_code('ebproduct.category')
         return records
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = f"[{record.category_id}] {record.category_name}"
+            result.append((record.id, name))
+        return result
--- inventory_inherit/eb_product_template_code.py
+++ inventory_inherit/eb_product_template_code.py
@@ -5,12 +5,33 @@ class InheritProductTemplateType(models.Model):
     _inherit = 'product.template'
     _order = 'product_template_code desc'
 
-    product_template_code = fields.Char(string="Product Code", readonly=True)
+    product_template_code = fields.Char(string="Internal Reference", readonly=True)
     category_name = fields.Many2one('ebproduct.category', string="Category")
 
     @api.model
     def create(self, vals):
-        record = super(InheritProductTemplateType, self).create(vals)
-        record.product_template_code = self.env['ir.sequence'].next_by_code('product.template')
-        # record.default_code = record.product_template_code
-        return record
+        records = super(InheritProductTemplateType, self).create(vals)
+        for record in records:
+            record.product_template_code = self.env['ir.sequence'].next_by_code('product.template')
+        return records
+
+
+
+from odoo import fields, models, api
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    common_product_code = fields.Char(string="Common Product Code", readonly=True)
+
+    @api.model
+    def create(self, vals):
+        vals['common_product_code'] = self.env['ir.sequence'].next_by_code('common.product.code.sequence')
+        return super(ProductTemplate, self).create(vals)
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.model
+    def _get_variant_default_code(self, product_template, product_attribute_value_ids):
+        return product_template.common_product_code
