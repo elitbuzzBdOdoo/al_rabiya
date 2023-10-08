e2e45a8d57cd17d0ba52d00ead6f5f7ffecf175e
arefin_shawon <arefin.shawon07@gmail.com>
2023-10-08 11:10:04 +0600

    purchase sequence updated

--- purchase_inherit/purchase_type.py
+++ purchase_inherit/purchase_type.py
@@ -3,16 +3,25 @@ from odoo import fields, models, api
 
 class PurchaseType(models.Model):
     _name = 'purchase.type'
+    _inherit = ['mail.thread']
+    _order = 'purchase_id desc'
     _rec_name = 'purchase_type'
     _description = 'purchase type'
 
     purchase_id = fields.Char(string="Purchase Code", readonly=True)
-    purchase_type = fields.Char(string="Purchase type", required=True)
+    purchase_type = fields.Char(string="Purchase type", required=True, track_visibility='always')
+
+    # @api.model
+    # def create(self, vals):
+    #     record = super().create(vals)
+    #     if record:
+    #         name_text = 'PT-0' + str(record.id)
+    #         record.update({'purchase_id': name_text})
+    #     return record
 
     @api.model
     def create(self, vals):
-        record = super().create(vals)
-        if record:
-            name_text = 'PT-0' + str(record.id)
-            record.update({'purchase_id': name_text})
-        return record
+        records = super(PurchaseType, self).create(vals)
+        for record in records:
+            record.purchase_id = self.env['ir.sequence'].next_by_code('purchase.type')
+        return records
